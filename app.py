e3c7dab750c5169805c063061b6f391d65723f37
Heesoo Kim <soyamilk0705@naver.com>
2020-11-01 22:44:38 +0900

    app.py 업로드

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask import render_template
+from models import db
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register():
+    pass
+
+
+@app.route('/control')
+def control():
+    pass
+
+
+@app.route('/login')
+def login():
+    pass
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/detail')
+def detail():
+    pass
+
+
+@app.route('/adminadd')
+def adminadd():
+    pass
+
+
+@app.route('/admin')
+def admin():
+    pass
+
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://" 
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+# app.config['SECRET_KEY'] = 'sdfsdfsdddf'
+# app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30) # 세션유지 시간을 30분으로 지정
+
+db.init_app(app)
+db.app = app
+db.create_all()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
