cc55df0137d4a827385718f5a745e6d0b590bce7
Heesoo Kim <soyamilk0705@naver.com>
2020-11-06 01:11:39 +0900

    app.py modify

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 from models import db
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../H2J2-Front', static_folder='../H2J2-Front/assets')
+CORS(app)
 
 @app.route('/')
 def index():
@@ -50,9 +52,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config['SECRET_KEY'] = 'sdfsdfsdddf'
 # app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30) # 세션유지 시간을 30분으로 지정
 
-db.init_app(app)
-db.app = app
-db.create_all()
+# db.init_app(app)
+# db.app = app
+# db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run()
+
+# host='127.0.0.1', port=5000, debug=True
+# host='0.0.0.0', port=5000, debug=True
